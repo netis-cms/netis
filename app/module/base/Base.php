a19cbf9f70c0301458dd0e658ba037c379cb5f22
Zdeněk Papučík <zdenek.papucik@gmail.com>
2018-06-16 14:36:49 +0200

    Fix

--- app/module/base/Base.php
+++ app/module/base/Base.php
@@ -20,7 +20,7 @@ use Nette\Application\UI;
 abstract class BasePresenter extends UI\Presenter
 {
 	use Drago\Application\UI\Drago;
-	use Localization\Locales;
+	use Localization\Locale;
 	use Application\UI\Factory;
 
 	/**
