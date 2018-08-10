32e64116ff59bcd429182ad3af8dae9451ffb45e
accgit <zdenek.papucik@gmail.com>
2018-08-10 10:04:20 +0200

    Added class Profile

---
+++ app/module/admin/presenter/Profile.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Netis, Little CMS
+ * Copyright (c) 2015, Zdeněk Papučík
+ */
+namespace Module\Admin;
+
+/**
+ * User profile.
+ */
+final class ProfilePresenter extends DashboardPresenter
+{
+
+}
