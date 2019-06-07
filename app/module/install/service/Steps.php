ed156715ebd8899573f1002ad3d3ed46d4a64572
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-06-07 06:31:41 +0200

    Update Steps.php

--- app/module/install/service/Steps.php
+++ app/module/install/service/Steps.php
@@ -1,31 +1,25 @@
 <?php
 
-/**
- * Netis, Little CMS
- * Copyright (c) 2015, Zdeněk Papučík
- */
+declare(strict_types = 1);
 
 namespace Module\Install\Service;
 
-use Nette\Caching;
+use Nette\Caching\Cache;
+
 
 /**
  * Saving installation steps into cache.
  */
 class Steps
 {
-	/**
-	 * @var Caching\Cache
-	 */
+	/** @var Cache */
 	public $cache;
 
-	/**
-	 * Install steps.
-	 */
+	/** Install steps. */
 	const STEP = 'Install step';
 
 
-	public function __construct(Caching\Cache $cache)
+	public function __construct(Cache $cache)
 	{
 		$this->cache = $cache;
 	}
