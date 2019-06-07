0eb53111372c7c00d6cad43f8da59c363413c142
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-06-07 06:31:01 +0200

    Update Router.php

--- app/module/install/Router.php
+++ app/module/install/Router.php
@@ -1,32 +1,23 @@
 <?php
 
-/**
- * Netis, Little CMS
- * Copyright (c) 2015, Zdeněk Papučík
- */
-
 namespace Module\Install;
 
 use Nette;
 use Nette\Application\Routers;
 
-/**
- * Router factory.
- */
+
 class Router
 {
 	use Nette\StaticClass;
 
-	/**
-	 * @param string $locales
-	 * @return Nette\Application\IRouter
-	 */
-	public static function create($locales)
+
+	public static function create(): Nette\Routing\Router
 	{
 		$router = new Routers\RouteList;
-		$router[] = $module = new Routers\RouteList('Install');
-		$module[] = new Routers\Route($locales . 'install/', 'Install:default');
+		$router
+			->withModule('Install')
+			->addRoute('[<lang=cs cs|en>/]install/', 'Install:default');
+
 		return $router;
 	}
-
 }
