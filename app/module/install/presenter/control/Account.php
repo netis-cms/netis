8d6df3f4a4fa81b3de0c6ce00046d5004ad08310
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-06-07 06:32:28 +0200

    Update Account.php

--- app/module/install/presenter/control/Account.php
+++ app/module/install/presenter/control/Account.php
@@ -1,89 +1,81 @@
 <?php
 
-/**
- * Netis, Little CMS
- * Copyright (c) 2015, Zdeněk Papučík
- */
+declare(strict_types = 1);
 
 namespace Module\Install\Control;
 
-use Drago;
-use Drago\Http;
+use Dibi;
+use Drago\Application\UI\Control;
+use Drago\Application\UI\Factory;
+use Drago\Http\Sessions;
+use Drago\Localization\TranslateControl;
+use Module\Install\Service\Query;
+use Module\Install\Service\Steps;
+use Nette\Application\UI\Form;
+use Nette\Security\Passwords;
 
-use Nette\Security;
-use Nette\Application\UI;
-use Module\Install\Service;
 
 /**
  * Add administrator account.
  */
-final class Account extends Drago\Application\UI\Control
+final class Account extends Control
 {
-	use Drago\Application\UI\Factory;
-	use Drago\Localization\TranslateControl;
+	use Factory;
+	use TranslateControl;
 
-	/**
-	 * @var Http\Sessions
-	 */
+	/** @var Sessions */
 	private $sessions;
 
-	/**
-	 * @var Service\Steps
-	 */
+	/** @var Steps */
 	private $steps;
 
-	/**
-	 * @var Service\Query
-	 */
+	/** @var Query  */
 	private $query;
 
+	/** @var Passwords */
+	private $password;
 
-	public function __construct(
-		Http\Sessions $sessions,
-		Service\Steps $steps,
-		Service\Query $query)
+
+	public function __construct(Sessions $sessions, Steps $steps, Query $query, Passwords $password)
 	{
-		parent::__construct();
 		$this->sessions = $sessions;
 		$this->steps = $steps;
 		$this->query = $query;
+		$this->password = $password;
 	}
 
 
-	public function render()
+	public function render(): void
 	{
 		$template = $this->template;
 		$template->setFile(__DIR__ . '/../templates/Control.account.latte');
-		$template->setTranslator($this->translation);
+		$template->setTranslator($this->getTranslator());
 		$template->form = $this['account'];
 		$template->render();
 	}
 
 
-	/**
-	 * @return UI\Form
-	 */
-	public function createComponentAccount()
+	public function createComponentAccount(): Form
 	{
 		$form = $this->createForm();
-		$form->setTranslator($this->translation);
+		$form->setTranslator($this->getTranslator());
 
 		$form->addText('realname', 'form.name.acc')
 			->setRequired('form.required');
 
 		$form->addText('email', 'form.email')
 			->setDefaultValue('@')
-			->setType('email')
+			->setHtmlType('email')
 			->setRequired('form.required')
-			->addRule(UI\Form::EMAIL, 'form.email.rule');
+			->addRule(Form::EMAIL, 'form.email.rule');
 
 		$form->addPassword('password', 'form.password')
 			->setRequired('form.required')
-			->addRule(UI\Form::MIN_LENGTH, 'form.password.rule', 6);
+			->addRule(Form::MIN_LENGTH, 'form.password.rule', 6);
 
 		$form->addPassword('verify', 'form.password.verify')
 			->setRequired('form.required')
-			->addRule(UI\Form::EQUAL, 'form.password.verify.rule', $form['password']);
+			->addRule(Form::EQUAL, 'form.password.verify.rule', $form['password']);
 
 		$form->addSubmit('send', 'form.send.acc');
 		$form->onSuccess[] = [$this, 'success'];
@@ -91,13 +83,16 @@ final class Account extends Drago\Application\UI\Control
 	}
 
 
-	public function success(UI\Form $form)
+	/**
+	 * @throws Dibi\Exception
+	 */
+	public function success(Form $form): void
 	{
 		$values = $form->getValues();
 		$table = $this->sessions->getSessionSection()->prefix . 'users';
 
 		// Hash password.
-		$values->password = Security\Passwords::hash($values->password);
+		$values->password = $this->password->hash($values->password);
 
 		// Undo unneeded values.
 		unset($values->verify, $values->prefix);
@@ -106,7 +101,7 @@ final class Account extends Drago\Application\UI\Control
 		$this->query->addRecord($table, $values);
 
 		// Save the installation step.
-		$this->steps->cache->save(Service\Steps::STEP, ['step' => 5]);
-		$this->flashMessage('message.acc', 'success');
+		$this->steps->cache->save(Steps::STEP, ['step' => 5]);
+		$this->presenter->flashMessage('message.acc', 'success');
 	}
 }
