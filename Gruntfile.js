602a6cb9cff34da2b832f9da275b4dc7322fc666
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-06-09 08:42:56 +0200

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,6 +21,13 @@ module.exports = function (grunt) {
                         expand: true, cwd: 'node_modules/font-awesome/fonts/',
                         src: ['**'],
                         dest: 'www/fonts/'
+                    },
+
+                    {
+                        expand: true,
+                        cwd: 'node_modules/open-sans-fontface/fonts/',
+                        src: ['**'],
+                        dest: 'www/css/fonts/'
                     }
                 ]
             }
@@ -28,15 +35,24 @@ module.exports = function (grunt) {
         concat: {
             css: {
                 files: {
-                    'assets/combined/css/install.css':  [
+                    'assets/combined/css/base.css':  [
                         'assets/css/bootstrap.css',
                         'assets/css/base.css',
+                        'node_modules/open-sans-fontface/open-sans.css',
+                        'node_modules/font-awesome/css/font-awesome.css'
+                    ],
+                    'assets/combined/css/install.css':  [
+                        'assets/combined/css/base.css',
                         'assets/css/install.css'
                     ],
                     'assets/combined/css/sign.css':     [
-                        'assets/css/bootstrap.css',
-                        'assets/css/base.css',
+                        'assets/combined/css/base.css',
                         'assets/css/sign.css'
+                    ],
+                    'assets/combined/css/admin.css':     [
+                        'assets/combined/css/base.css',
+                        'node_modules/perfect-scrollbar/css/perfect-scrollbar.css',
+                        'node_modules/sidebar-menu-compostrap/dist/css/sidebar.css'
                     ],
                 },
             },
@@ -49,13 +65,19 @@ module.exports = function (grunt) {
                         'node_modules/nette.ajax.js/nette.ajax.js',
                         'node_modules/live-form-validation/live-form-validation.js'
                     ],
+                    'assets/combined/js/admin.js':  [
+                        'assets/combined/js/app.js',
+                        'node_modules/nanobar/nanobar.js',
+                        'node_modules/sidebar-menu-compostrap/dist/js/sidebar.menu.js'
+                    ],
                 },
             }
         },
         uglify: {
             dist: {
                 files: {
-                    'www/js/app.min.js': ['assets/combined/js/app.js']
+                    'www/js/app.min.js':   ['assets/combined/js/app.js'],
+                    'www/js/admin.min.js': ['assets/combined/js/admin.js']
                 },
             }
         },
@@ -63,7 +85,8 @@ module.exports = function (grunt) {
             dist: {
                 files: {
                     'www/css/install.min.css': ['assets/combined/css/install.css'],
-                    'www/css/sign.min.css':    ['assets/combined/css/sign.css']
+                    'www/css/sign.min.css':    ['assets/combined/css/sign.css'],
+                    'www/css/admin.min.css':   ['assets/combined/css/admin.css']
                 },
             }
         },
@@ -71,7 +94,8 @@ module.exports = function (grunt) {
             dist: {
                 files: {
                     'app/module/install/presenter/templates/@layout.latte':    ['app/module/install/presenter/templates/@dev.latte'],
-                    'app/module/admin/presenter/templates/Sign/@layout.latte': ['app/module/admin/presenter/templates/Sign/@dev.latte']
+                    'app/module/admin/presenter/templates/Sign/@layout.latte': ['app/module/admin/presenter/templates/Sign/@dev.latte'],
+                    'app/module/admin/presenter/templates/Admin/@layout.latte': ['app/module/admin/presenter/templates/Admin/@dev.latte']
                 }
             }
         },
