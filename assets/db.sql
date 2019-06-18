b2cc11a830ddfa502b14922db6c2c166877a89d4
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-06-18 14:01:03 +0200

    Update db.sql

--- assets/db.sql
+++ assets/db.sql
@@ -11,16 +11,18 @@ CREATE TABLE `menu_category` (
 
 
 -- ---- insert values to table:
-INSERT INTO `menu_category` (`categoryId`, `category`)
-VALUES (1, 'category.system');
+INSERT INTO `menu_category` (`categoryId`, `category`) VALUES
+(1, 'category.system');
 
 
 -- ---- create table:
 CREATE TABLE `menu` (
   `menuId` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `categoryId` int(11) unsigned NOT NULL,
+  `parent` int(11) NOT NULL,
   `link` char(30) NOT NULL,
   `name` char(30) NOT NULL,
+  `icon` char(30) NOT NULL,
   PRIMARY KEY (`menuId`),
   KEY `category` (`categoryId`),
   CONSTRAINT `menu` FOREIGN KEY (`categoryId`)
@@ -29,10 +31,9 @@ CREATE TABLE `menu` (
 
 
 -- ---- insert values to table:
-INSERT INTO `menu` (`menuId`, `categoryId`, `link`, `name`)
-VALUES
-(1, 1, ':Admin:Admin:main', 'menu.admin'),
-(2, 1, ':Admin:Settings:web', 'menu.settings');
+INSERT INTO `menu` (`menuId`, `categoryId`, `parent`, `link`, `name`, `icon`) VALUES
+(1, 1, '0', ':Admin:Admin:main', 'menu.admin', 'fa fa-home'),
+(2, 1, '0', ':Admin:Settings:web', 'menu.settings', 'fa fa-cog');
 
 
 -- ---- create table:
@@ -40,6 +41,12 @@ CREATE TABLE `settings` (
   `name` varchar(100) NOT NULL,
   `value` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ---- insert values to table:
+INSERT INTO `settings` (`name`, `value`) VALUES
+('website', 'Netis'),
+('description', 'CMS');
 
 
 -- ---- create table:
@@ -50,3 +57,8 @@ CREATE TABLE `users` (
   `password` char(60) NOT NULL,
   PRIMARY KEY (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- ---- insert values to table:
+INSERT INTO `users` (`userId`, `realname`, `email`, `password`) VALUES
+(1,	'Admin',	'admin@local',	'$2y$10$Gqo0SEC.DUEhEpRt//kpOum2ki7ueECrI/jGBEnI/sfPXOe4ZYeoC');
