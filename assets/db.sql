d076d1b6f2485076c58dbe5da1dfd9161ee96add
accgit <zdenek.papucik@gmail.com>
2021-04-30 08:17:25 +0200

    Fix

--- assets/db.sql
+++ assets/db.sql
@@ -9,34 +9,6 @@ SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 -- ---- create table:
-CREATE TABLE `settings` (
-    `name` varchar(100) NOT NULL,
-    `value` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ---- create table:
-CREATE TABLE `users` (
-    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `username` varchar(50) NOT NULL,
-    `email` varchar(50) NOT NULL,
-    `password` varchar(60) NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ---- create table:
-CREATE TABLE `authorization` (
-    `role_id` int(11) unsigned NOT NULL,
-    `user_id` int(11) unsigned NOT NULL,
-     KEY `user` (`user_id`),
-     KEY `role` (`role_id`),
-     CONSTRAINT `acl_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-     CONSTRAINT `acl_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ---- create view:
-CREATE TABLE `authorization_view` (`role` varchar(40));
-
--- ---- create table:
 CREATE TABLE `permissions` (
     `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
     `role_id` int(11) unsigned NOT NULL,
@@ -80,7 +52,7 @@ CREATE TABLE `privileges` (
 INSERT INTO `privileges` (`id`, `name`) VALUES
 (1,	'*all'),
 (2,	'default'),
-(2,	'submit');
+(3,	'submit');
 
 -- ---- create table:
 CREATE TABLE `resources` (
@@ -117,26 +89,53 @@ CREATE TRIGGER `insert` BEFORE INSERT ON `roles` FOR EACH ROW
 BEGIN
     IF (new.name = 'admin') THEN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'This name is not allowed for the role.';
-    END IF;
+END IF;
 END;;
 
 CREATE TRIGGER `update` BEFORE UPDATE ON `roles` FOR EACH ROW
 BEGIN
     IF (new.name = 'admin') THEN
     SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'The role is not allowed to change to this name.';
-    END IF;
+END IF;
 END;;
 
 DELIMITER ;
 
--- ---- create query:
-DROP TABLE IF EXISTS `authorization_view`;
-CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `authorization_view` AS select `r`.`name` AS `role` from (`authorization` `a` left join `roles` `r` on(`a`.`role_id` = `r`.`id`));
+-- ---- create table:
+CREATE TABLE `settings` (
+    `name` varchar(100) NOT NULL,
+    `value` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ---- create table:
+CREATE TABLE `users` (
+    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+    `username` varchar(50) NOT NULL,
+    `email` varchar(50) NOT NULL,
+    `password` varchar(60) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ---- create table:
+CREATE TABLE `users_roles` (
+    `role_id` int(11) unsigned NOT NULL,
+    `user_id` int(11) unsigned NOT NULL,
+    KEY `user` (`user_id`),
+    KEY `role` (`role_id`),
+    CONSTRAINT `users_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+    CONSTRAINT `users_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ---- create view:
+CREATE TABLE `users_roles_view` (
+    `role` varchar(40)
+);
 
 -- ---- create query:
 DROP TABLE IF EXISTS `permissions_roles_view`;
 CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `permissions_roles_view` AS select `roles`.`id` AS `id`,`roles`.`name` AS `name`,`roles`.`parent` AS `parent`
 from `roles` where `roles`.`id` in (select distinct `permissions`.`role_id` from `permissions` where `roles`.`name` <> 'admin');
+
 
 -- ---- create query:
 DROP TABLE IF EXISTS `permissions_view`;
@@ -145,3 +144,8 @@ from (((`permissions` `p`
     left join `resources` `r` on(`p`.`resource_id` = `r`.`id`))
     left join `privileges` `p2` on(`p`.`privilege_id` = `p2`.`id`))
     left join `roles` `r2` on(`p`.`role_id` = `r2`.`id`));
+
+-- ---- create query:
+DROP TABLE IF EXISTS `users_roles_view`;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `users_roles_view` AS select `r`.`name` AS `role`
+from (`users_roles` `u` left join `roles` `r` on(`u`.`role_id` = `r`.`id`));
