9df1c4a1c69b6b56aff9b3130d5618335eea4d49
accgit <zdenek.papucik@gmail.com>
2021-05-03 17:59:22 +0200

    Update

--- app/module/admin/presenter/AccessControlPresenter.php
+++ app/module/admin/presenter/AccessControlPresenter.php
@@ -5,9 +5,59 @@ declare(strict_types=1);
 namespace Module\Admin;
 
 use Base\BackendPresenter;
-use Tracy\Debugger;
+use Drago\Authorization\Control\PermissionsControl;
+use Drago\Authorization\Control\PrivilegesControl;
+use Drago\Authorization\Control\ResourcesControl;
+use Drago\Authorization\Control\RolesControl;
+use Drago\Authorization\FileNotFoundException;
 
 
 final class AccessControlPresenter extends BackendPresenter
 {
+	/**
+	 * @throws FileNotFoundException
+	 */
+	protected function createComponentPermissionsControl(): PermissionsControl
+	{
+		$control = $this->permissionsControl;
+		$control->setTemplateFile(__DIR__ . '/templates/AccessControl/permissions.add.latte');
+		$control->setTemplateFile(__DIR__ . '/templates/AccessControl/permissions.records.latte', 'records');
+		return $control;
+	}
+
+
+	/**
+	 * @throws FileNotFoundException
+	 */
+	protected function createComponentRolesControl(): RolesControl
+	{
+		$control = $this->rolesControl;
+		$control->setTemplateFile(__DIR__ . '/templates/AccessControl/roles.add.latte');
+		$control->setTemplateFile(__DIR__ . '/templates/AccessControl/roles.records.latte', 'records');
+		return $control;
+	}
+
+
+	/**
+	 * @throws FileNotFoundException
+	 */
+	protected function createComponentResourcesControl(): ResourcesControl
+	{
+		$control = $this->resourcesControl;
+		$control->setTemplateFile(__DIR__ . '/templates/AccessControl/resources.add.latte');
+		$control->setTemplateFile(__DIR__ . '/templates/AccessControl/resources.records.latte', 'records');
+		return $control;
+	}
+
+
+	/**
+	 * @throws FileNotFoundException
+	 */
+	protected function createComponentPrivilegesControl(): PrivilegesControl
+	{
+		$control = $this->privilegesControl;
+		$control->setTemplateFile(__DIR__ . '/templates/AccessControl/privileges.add.latte');
+		$control->setTemplateFile(__DIR__ . '/templates/AccessControl/privileges.records.latte', 'records');
+		return $control;
+	}
 }
