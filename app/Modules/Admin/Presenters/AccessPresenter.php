4f6765b6cbf8055d3f169653c7533c0ea73a64a3
accgit <zdenek.papucik@gmail.com>
2022-05-03 11:40:57 +0200

    Update

--- app/Modules/Admin/Presenters/AccessPresenter.php
+++ app/Modules/Admin/Presenters/AccessPresenter.php
@@ -10,57 +10,56 @@ use Drago\Authorization\Control\Permissions\PermissionsControl;
 use Drago\Authorization\Control\Privileges\PrivilegesControl;
 use Drago\Authorization\Control\Resources\ResourcesControl;
 use Drago\Authorization\Control\Roles\RolesControl;
-use Drago\Authorization\FileNotFoundException;
 
 
 final class AccessPresenter extends DashboardPresenter
 {
-	/**
-	 * @throws FileNotFoundException
-	 */
 	protected function createComponentPermissionsControl(): PermissionsControl
 	{
 		$control = $this->permissionsControl;
+		$control->templateFactory = __DIR__ . '/../../../UI/Templates/Permissions/Permissions.latte';
+		$control->templateItems = __DIR__ . '/../../../UI/Templates/Permissions/PermissionsItems.latte';
+		$control->translator = $this->getTranslator();
 		return $control;
 	}
 
 
-	/**
-	 * @throws FileNotFoundException
-	 */
 	protected function createComponentRolesControl(): RolesControl
 	{
 		$control = $this->rolesControl;
+		$control->templateFactory = __DIR__ . '/../../../UI/Templates/Roles/Roles.latte';
+		$control->templateItems = __DIR__ . '/../../../UI/Templates/Roles/RolesItems.latte';
+		$control->translator = $this->getTranslator();
 		return $control;
 	}
 
 
-	/**
-	 * @throws FileNotFoundException
-	 */
 	protected function createComponentResourcesControl(): ResourcesControl
 	{
 		$control = $this->resourcesControl;
+		$control->templateFactory = __DIR__ . '/../../../UI/Templates/Resources/Resources.latte';
+		$control->templateItems = __DIR__ . '/../../../UI/Templates/Resources/ResourcesItems.latte';
+		$control->translator = $this->getTranslator();
 		return $control;
 	}
 
 
-	/**
-	 * @throws FileNotFoundException
-	 */
 	protected function createComponentPrivilegesControl(): PrivilegesControl
 	{
 		$control = $this->privilegesControl;
+		$control->templateFactory = __DIR__ . '/../../../UI/Templates/Privileges/Privileges.latte';
+		$control->templateItems = __DIR__ . '/../../../UI/Templates/Privileges/PrivilegesItems.latte';
+		$control->translator = $this->getTranslator();
 		return $control;
 	}
 
 
-	/**
-	 * @throws FileNotFoundException
-	 */
 	protected function createComponentAccessControl(): AccessControl
 	{
 		$control = $this->accessControl;
+		$control->templateFactory = __DIR__ . '/../../../UI/Templates/Access/Access.latte';
+		$control->templateItems = __DIR__ . '/../../../UI/Templates/Access/AccessItems.latte';
+		$control->translator = $this->getTranslator();
 		return $control;
 	}
 }
