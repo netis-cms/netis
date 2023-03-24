85a5d4c9da37d8ca713ac929c4970c815f7b3f41
accgit <zdenek.papucik@gmail.com>
2023-03-24 11:35:28 +0100

    Update

--- public/js/install.js
+++ public/js/install.js
@@ -3,3 +3,14 @@ import "../styles/install.scss";
 
 /* js */
 import "../base";
+
+const button = document.getElementById('btn-send');
+if (button) {
+	button.addEventListener('click', (e) => {
+		const url = e.target.getAttribute('data-url');
+		button.disabled = true;
+		naja.makeRequest('GET', url, null, {
+			history: false,
+		}).then();
+	});
+}
