98b3936dec2092b6a47f53e105d0d862566ff6c5
Zdeněk Papučík <zdenek.papucik@gmail.com>
2023-03-24 13:51:05 +0100

    Update AccessTemplate.php

--- app/Modules/Backend/Access/AccessTemplate.php
+++ app/Modules/Backend/Access/AccessTemplate.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Modules\Backend\Access;
 
 use App\Modules\Backend\BackendTemplate;
-use Nette\ComponentModel\IComponent;
 
 
 final class AccessTemplate extends BackendTemplate
