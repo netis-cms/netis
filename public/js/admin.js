07bbfa31ce337d7a2d4d482136f0715190a8c930
accgit <zdenek.papucik@gmail.com>
2023-08-02 10:40:32 +0200

    dev

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,18 +5,59 @@ import "../styles/admin.scss";
 import "../base";
 import "sidebar-skeleton-compostrap";
 import "sidebar-menu-compostrap";
-import "../../vendor/ublaboo/datagrid/assets/datagrid";
-import "../../vendor/ublaboo/datagrid/assets/datagrid-instant-url-refresh";
 import "../naja.components";
 import "../naja.tom.select";
+import Select from "tom-select";
 import PerfectScrollbar from "perfect-scrollbar";
-
-new PerfectScrollbar('.scrollbar', {
-	wheelSpeed: 0.3
-});
+import {
+	AutosubmitPlugin,
+	CheckboxPlugin,
+	ConfirmPlugin,
+	createDatagrids,
+	InlinePlugin,
+	ItemDetailPlugin,
+	NetteFormsPlugin,
+	SelectpickerPlugin,
+	SortableJS,
+	SortablePlugin,
+	TomSelect
+} from "../datagrid";
+import {TreeViewPlugin} from "../datagrid/plugins/features/treeView";
+import {NajaAjax} from "../datagrid/ajax";
 
 /* bootstrap alert */
 const alertList = document.querySelectorAll('.alert');
 const alerts = [].slice.call(alertList).map(function (element) {
 	return new bootstrap.Alert(element);
+});
+
+/* scrollbar */
+new PerfectScrollbar('.scrollbar', {
+	wheelSpeed: 0.3
+});
+
+/* datagrid */
+function datagridInit(){
+	createDatagrids(new NajaAjax(naja), {
+		datagrid: {
+			plugins: [
+				new AutosubmitPlugin(),
+				new CheckboxPlugin(),
+				new ConfirmPlugin(),
+				new InlinePlugin(),
+				new ItemDetailPlugin(),
+				new NetteFormsPlugin(Nette),
+				new SortablePlugin(new SortableJS()),
+				new SelectpickerPlugin(new TomSelect(Select)),
+				new TreeViewPlugin(),
+			],
+		},
+	});
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+	naja.formsHandler.netteForms = Nette;
+	naja.initialize();
+	datagridInit();
 });
