fa0319bf6246de0adc4e3aef9993e97e526c9d46
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-01-22 08:13:08 +0100

    Update naja.confirm.js

--- assets/naja.confirm.js
+++ assets/naja.confirm.js
@@ -3,7 +3,7 @@ export default class ConfirmExtension {
 		naja.uiHandler.addEventListener('interaction', (e) => {
 			const el = e.detail.element;
 			const confirmMessage = el.getAttribute('data-confirm') || el.getAttribute('data-datagrid-confirm');
-			
+
 			// Pokud má element nějaký confirm atribut a uživatel nepotvrdí
 			if (confirmMessage && !window.confirm(confirmMessage)) {
 				e.preventDefault();
