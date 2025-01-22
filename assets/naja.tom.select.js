c1c5744788a9bc79ad5167ab7c51a3c94026ebdc
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-01-22 07:13:13 +0100

    Update naja.tom.select.js

--- assets/naja.tom.select.js
+++ assets/naja.tom.select.js
@@ -2,18 +2,19 @@ import naja from "naja";
 import TomSelect from "tom-select";
 
 function tomSelect(el) {
-	for(let e of el.querySelectorAll('.select-tom')) {
-		let isLocked = e.hasAttribute('data-locked');
-		e._tom = new TomSelect(e, {
-			plugins: isLocked? []:['remove_button'],
-		});
-		if (isLocked) {
-			e._tom.lock();
-		}
-	}
+  el.querySelectorAll('.select-tom').forEach(e => {
+    const isLocked = e.hasAttribute('data-locked');
+    e._tom = new TomSelect(e, {
+      plugins: isLocked ? [] : ['remove_button'],
+    });
+    
+    if (isLocked) {
+      e._tom.lock();
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => tomSelect(document));
 naja.snippetHandler.addEventListener('afterUpdate', (e) => {
-	tomSelect(e.detail.snippet);
+  tomSelect(e.detail.snippet);
 });
