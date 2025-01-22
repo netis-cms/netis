6a43b1f02f9de63e5dfb3a5b88510e19cbbceb75
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-01-22 07:14:11 +0100

    Update naja.spinner.js

--- assets/naja.spinner.js
+++ assets/naja.spinner.js
@@ -3,13 +3,21 @@ export default class SpinnerExtension {
 	initialize(naja) {
 		const el = document.createElement('div');
 		el.classList.add('spinner');
+		document.body.appendChild(el); // Přidáme spinner do DOMu jen jednou
+
+		// Skrýváme spinner, dokud není potřeba
+		el.style.display = 'none';
+
 		naja.addEventListener('start', () => {
+			if (reqCnt === 0) {
+				el.style.display = 'block'; // Zobrazíme spinner při prvním požadavku
+			}
 			reqCnt++;
-			document.body.appendChild(el);
 		});
+
 		naja.addEventListener('complete', () => {
 			if (--reqCnt === 0) {
-				document.body.appendChild(el).remove();
+				el.style.display = 'none'; // Skryjeme spinner po dokončení posledního požadavku
 			}
 		});
 	}
