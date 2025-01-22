64e029006631601eef166669e88c330454fd695d
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-01-22 08:04:34 +0100

    Update naja.components.js

--- assets/naja.components.js
+++ assets/naja.components.js
@@ -3,49 +3,33 @@ import { Modal, Offcanvas } from "bootstrap";
 
 // Funkce pro inicializaci Bootstrap komponent
 function initBsComponents(el) {
-  // Funkce pro inicializaci jednotlivých komponent
-  function initComponent(component, ComponentClass) {
-    el.querySelectorAll(component).forEach((el) => {
-      if (!el._bsInstance) {
-        el._bsInstance = new ComponentClass(el, { keyboard: false });
-      }
-    });
-  }
+	// Funkce pro inicializaci jednotlivých komponent
+	function initComponent(component, ComponentClass) {
+		el.querySelectorAll(component).forEach((el) => {
+			if (!el._bsInstance) {
+				el._bsInstance = new ComponentClass(el, { keyboard: false });
+			}
+		});
+	}
 
-  // Inicializace modálů a offcanvas
-  initComponent('.modal', Modal);
-  initComponent('.offcanvas', Offcanvas);
+	// Inicializace modálů a offcanvas
+	initComponent('.modal', Modal);
+	initComponent('.offcanvas', Offcanvas);
 }
 
 document.addEventListener('DOMContentLoaded', () => initBsComponents(document));
 
 // Po každé aktualizaci snippetů
 naja.snippetHandler.addEventListener('afterUpdate', (e) => {
-  initBsComponents(e.detail.snippet);
+	initBsComponents(e.detail.snippet);
 });
 
 // Po dokončení požadavku (např. po úspěšném odeslání formuláře)
 naja.addEventListener('complete', (e) => {
-  if (e.detail.payload) {
-    const doc = document;
-    const { modal, offcanvas, close } = e.detail.payload;
+	if (e.detail.payload) {
+		const doc = document;
+		const { modal, offcanvas, close } = e.detail.payload;
 
-    // Zobrazíme modal, pokud je uveden v payload
-    if (modal) {
-      const targetModal = doc.querySelector(`#${modal}`) || doc.querySelector(`#${modal} .modal`);
-      targetModal?._bsModal?.show();
-    }
-
-    // Zobrazíme offcanvas, pokud je uveden v payload
-    if (offcanvas) {
-      const targetOffcanvas = doc.querySelector(`#${offcanvas}`) || doc.querySelector(`#${offcanvas} .offcanvas`);
-      targetOffcanvas?._bsOffcanvas?.show();
-    }
-
-    // Zavřeme otevřený modal nebo offcanvas
-    if (close) {
-      doc.querySelector('.offcanvas.show')?._bsOffcanvas?.hide();
-      doc.querySelector('.modal.show')?._bsModal?.hide();
-    }
-  }
-});
+		// Zobrazíme modal, pokud je uveden v payload
+		if (modal) {
+			const targetModal = doc.querySelector(`#${mo
