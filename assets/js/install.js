b8d301d6d6ab7b3c59fdadec2926baa5f940ba48
Zdeněk Papučík <zdenek.papucik@gmail.com>
2025-01-22 08:03:10 +0100

    Update install.js

--- assets/js/install.js
+++ assets/js/install.js
@@ -10,22 +10,22 @@ document.addEventListener('DOMContentLoaded', () => naja.initialize());
 
 /* Inicializace Bootstrap alert komponent */
 document.querySelectorAll('.alert').forEach((element) => {
-  new Alert(element);
+	new Alert(element);
 });
 
 /* Odesílání GET požadavku při kliknutí na tlačítko */
 const button = document.getElementById('btn-send');
 if (button) {
-  button.addEventListener('click', (e) => {
-    const url = e.target.dataset.url; // Použijeme dataset pro lepší čitelnost
-    button.disabled = true;
+	button.addEventListener('click', (e) => {
+		const url = e.target.dataset.url; // Použijeme dataset pro lepší čitelnost
+		button.disabled = true;
 
-    // Odeslání GET požadavku pomocí Naja
-    naja.makeRequest('GET', url, null, { history: false }).then(() => {
-      // Pokud je třeba nějaké další zpracování po dokončení požadavku, můžeme to přidat zde.
-    }).catch(() => {
-      // Volitelné: ošetření chyb, pokud je potřeba.
-      button.disabled = false; // Znovu povolíme tlačítko v případě chyby
-    });
-  });
+		// Odeslání GET požadavku pomocí Naja
+		naja.makeRequest('GET', url, null, { history: false }).then(() => {
+			// Pokud je třeba nějaké další zpracování po dokončení požadavku, můžeme to přidat zde.
+		}).catch(() => {
+			// Volitelné: ošetření chyb, pokud je potřeba.
+			button.disabled = false; // Znovu povolíme tlačítko v případě chyby
+		});
+	});
 }
